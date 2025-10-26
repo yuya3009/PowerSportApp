86a787e1dbc83a16d17841f26a7c6be92591a8bb
yuya yokobori <yoko3057co@gmail.com>
2025-10-26 17:18:32 +0900

    列数を動的にするよう実装&知り合い経路の選択リスト値を増加

--- force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
+++ force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
@@ -1,5 +1,23 @@
 import { LightningElement } from 'lwc';
 
 export default class BulkRegistTargetAudience extends LightningElement {
-  
+  // デフォルトの行数
+  rowIndex = 3;
+  // 項目ごとに列を定義
+  rows = [
+    { id: 1, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] },
+    { id: 2, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] },
+    { id: 3, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] }
+  ];
+
+  /**
+   * Idをキーに追加・削除するのでIdの数を動的に変化させる
+   */
+  addRow() {
+    this.rowIndex++;
+    this.rows = [
+      ...this.rows,
+      { id: this.rowIndex, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] }
+    ];
+  }
 }
