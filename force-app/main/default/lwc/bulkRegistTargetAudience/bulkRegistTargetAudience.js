ceda12bb52266ece056ee849abacb07a64fd48ac
yuya yokobori <yoko3057co@gmail.com>
2025-10-26 22:13:48 +0900

    utilクラスを作成&一部項目を必須化

--- force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
+++ force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
@@ -1,13 +1,16 @@
 import { LightningElement } from 'lwc';
+import { displayToast } from 'c/util';
 
 export default class BulkRegistTargetAudience extends LightningElement {
   // デフォルトの行数
   rowIndex = 3;
+  // 項目の配列
+  fieldRows = ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'];
   // 項目ごとに列を定義
   rows = [
-    { id: 1, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] },
-    { id: 2, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] },
-    { id: 3, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] }
+    { id: 1, fields: this.fieldRows },
+    { id: 2, fields: this.fieldRows },
+    { id: 3, fields: this.fieldRows }
   ];
 
   /**
@@ -17,7 +20,19 @@ export default class BulkRegistTargetAudience extends LightningElement {
     this.rowIndex++;
     this.rows = [
       ...this.rows,
-      { id: this.rowIndex, fields: ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'] }
+      { id: this.rowIndex, fields: this.fieldRows }
     ];
+  }
+
+  handleCreate() {
+    /**
+     * 入力項目判定
+     * 仕様として、入力されていない行も存在するので、入力されている項目が1~3個の場合はエラーを表示する
+     * 0または4の場合はinsertする
+     */
+    console.log("列項目", this.rows);
+    this.rows.forEach((row) => {
+// ToDo 項目値があるか判定
+    });
   }
 }
---
+++ force-app/main/default/lwc/util/util.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Util extends LightningElement {
+  @api displayToast(title, message, variant) {
+    const event = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant,
+    });
+    this.dispatchEvent(event);
+  }
+}
