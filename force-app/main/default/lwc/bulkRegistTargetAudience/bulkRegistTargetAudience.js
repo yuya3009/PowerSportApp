6a74f066b11ee7dc44ca9193d1b6f72fea25e471
yuya yokobori <yoko3057co@gmail.com>
2025-12-06 16:05:36 +0900

    登録機能が完成

--- force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
+++ force-app/main/default/lwc/bulkRegistTargetAudience/bulkRegistTargetAudience.js
@@ -1,11 +1,12 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import registTargetAudience from '@salesforce/apex/targetAudienceHandler.createTargetAudience';
 
 export default class BulkRegistTargetAudience extends LightningElement {
   // デフォルトの行数
   rowIndex = 3;
   // 項目の配列
-  fieldRows = ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c'];
+  fieldRows = ['Name', 'AcquaintanceRoute__c', 'LineInformation__c', 'FirstConversationDate__c', 	'edgeAccuracy__c'];
   // 項目ごとに列を定義
   rows = [
     { id: 1, fields: this.fieldRows },
@@ -48,16 +49,31 @@ export default class BulkRegistTargetAudience extends LightningElement {
     /**
      * 入力項目判定
      * handleTargetAudienceで作られた配列から判定する
+     * キー(項目API名)がなかったら、エラーを表示する
      */
     let fieldName_name = 'Name';
     let fieldName_AcquaintanceRoute__c = 'AcquaintanceRoute__c';
     let fieldName_LineInformation__c = 'LineInformation__c';
     let fieldName_FirstConversationDate__c = 'FirstConversationDate__c';
     let failureOfToast = 'error';
-    // this.targetRow = JSON.stringify(this.targetRow);
-    Object.keys(this.targetRow).forEach(rowNumber => {
-      // 各項目の値をチェック
-      let row = this.targetRow[rowNumber];
+    // Apexに渡す対象者レコード
+    let targetAudience = [];
+    // 何も入力されていなかったら、エラーメッセージを表示
+    if (Object.keys(this.targetRow).length === 0) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: '',
+          message: '1行以上入力してから登録してください',
+          variant: failureOfToast
+        })
+      );
+      return;
+    }
+    Object.values(this.targetRow).forEach(row => {
+      /**
+       * 入信確度以外の項目の値をチェック
+       * 入信確度は設定で必須項目になっていないため、入信確度は必須にしない。
+       */
       // Nameバリデーション
       if (!(fieldName_name in row)) {
         this.dispatchEvent(
@@ -102,6 +118,30 @@ export default class BulkRegistTargetAudience extends LightningElement {
         );
         return;
       }
+      targetAudience.push(row);
+    });
+console.log('targetAudience :', targetAudience);
+console.log('文字列化したtargetAudience', JSON.stringify(targetAudience));
+    registTargetAudience({ targetAudience: targetAudience })
+    .then((result) => {
+      console.log('登録成功結果', result);
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: '',
+          message: '連絡先情報を登録しました',
+          variant: 'success'
+        })
+      );
+    })
+    .catch((error) => {
+      console.log('登録失敗結果', error);
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: '',
+          message: '連絡先情報の登録に失敗しました。管理者に連絡してください。',
+          variant: failureOfToast
+        })
+      );
     });
   }
 }
